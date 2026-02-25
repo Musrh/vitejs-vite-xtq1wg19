53149010bc75904e2d32c8557ab56a858c2296d5
Musrh <musplay2023@gmail.com>
2026-02-25 07:04:40 +0000

    Create Produits.vue

---
+++ src/views/Produits.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <h1>Produits</h1>
+    <p>Voici la liste de nos produits :</p>
+    <!-- Tu peux mettre ton composant ou liste ici -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Produits"
+};
+</script>
+
+<style scoped>
+.page {
+  padding: 20px;
+}
+</style>
