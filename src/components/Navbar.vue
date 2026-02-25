c0f5390d82c7b2bfceedce9881f9ffb53496a1ae
Musrh <musplay2023@gmail.com>
2026-02-25 07:23:27 +0000

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,28 +4,3 @@
     <router-link to="/contact">Contact</router-link>
   </nav>
 </template>
-
-<script>
-export default {
-  name: "Navbar"
-};
-</script>
-
-<style scoped>
-.navbar {
-  display: flex;
-  gap: 20px;
-  padding: 15px;
-  background-color: var(--primary-color);
-}
-
-.navbar a {
-  color: white;
-  text-decoration: none;
-  font-weight: bold;
-}
-
-.navbar a.router-link-active {
-  text-decoration: underline;
-}
-</style>
