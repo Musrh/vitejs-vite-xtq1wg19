f566e9dc622b286cbf4a2925bcbb941658a51ea5
Musrh <musplay2023@gmail.com>
2026-02-25 07:07:40 +0000

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar">
+    <router-link to="/produits">Produits</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  gap: 20px;
+  padding: 15px;
+  background-color: var(--primary-color);
+}
+
+.navbar a {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.navbar a.router-link-active {
+  text-decoration: underline;
+}
+</style>
