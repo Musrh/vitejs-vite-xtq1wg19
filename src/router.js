f5b279cdd218bff2781de725d49e36cef1b64743
Musrh <musplay2023@gmail.com>
2026-02-25 18:14:55 +0000

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Contact from "./views/Contact.vue";
 import Produits from "./views/Produits.vue";
+import Accueil from "./views/Accueil.vue";
 
 const routes = [
   { path: "/contact", component: Contact },
   { path: "/produits", component: Produits },
+  {path:  "/accueil", component:  Accueil},
 ];
 
 const router = createRouter({
