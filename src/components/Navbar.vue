2e20bf0ebb665e5096b6b8fa5a2ce24838557ad9
Musrh <Musrh@users.noreply.github.com>
2026-02-25 07:51:11 +0000

    deploy: 8003fc30d41c62aca54a91c34a9c50160271bfe0

--- src/components/Navbar.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <nav class="navbar">
-    <router-link to="/produits">Produits</router-link>
-    <router-link to="/contact">Contact</router-link>
-  </nav>
-</template>
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// 🔹 Important : base doit correspondre au nom exact du repo GitHub
-export default defineConfig({
-  plugins: [vue()],
-  base: '/vitejs-vite-xtq1wg19/'  // ✅ nom exact du repo GitHub
-})
