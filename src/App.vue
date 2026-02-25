f7848b2791ef1aaf663898eb9137d67f7ff4c617
Musrh <musplay2023@gmail.com>
2026-02-25 07:18:49 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
-<script setup>
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view></router-view>
+  </div>
+</template>
 
+<script>
 import Navbar from "./components/Navbar.vue";
 
 export default {
   components: { Navbar }
 };
 </script>
-
-<template>
-<div id="app">
-    <Navbar />
-    <router-view></router-view>
-  </div>
-
-</template>
