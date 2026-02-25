390d6ed2b95b777ddd7270eb4705611ee5fa4bb7
Musrh <musplay2023@gmail.com>
2026-02-25 07:03:21 +0000

    Create Contact.vue

---
+++ src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page">
+    <h1>Contact</h1>
+    <p>Vous pouvez nous écrire à contact@monprijet.com</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact"
+};
+</script>
+
+<style scoped>
+.page {
+  padding: 20px;
+}
+</style>
