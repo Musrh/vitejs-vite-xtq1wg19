17690391d84e775c9637274da01b513ae07b63ed
Musrh <musplay2023@gmail.com>
2026-02-25 08:12:17 +0000

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,14 @@
 <template>
   <nav class="navbar">
     <router-link to="/produits">Produits</router-link>
-    |<router-link to="/contact">Contact</router-link>
+    <router-link to="/contact">Contact</router-link>
   </nav>
 </template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  gap: 25px; /* 👈 espace entre les items */
+  padding: 15px;
+}
+</style>
