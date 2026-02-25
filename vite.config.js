6c2e555b249b29b8966ff6a4a512127b1c2f1e3f
Musrh <musplay2023@gmail.com>
2026-02-25 07:37:10 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// 🔹 Important : base doit correspondre au nom exact du repo GitHub
 export default defineConfig({
   plugins: [vue()],
+  base: '/vitejs-vite-xtq1wg19/'  // ✅ nom exact du repo GitHub
 })
