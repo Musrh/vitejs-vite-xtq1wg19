bb091d2bc1a5f4d328d417dc23ce288898307ebe
Musrh <musplay2023@gmail.com>
2026-02-26 00:23:05 +0000

    Update Accueil.vue

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,4 +1,18 @@
 <template>
-<h3>Welcome to your Store</h3>
-
+  <div class="page">
+    <h1>Welcome to Our Store</h1>
+    <p>Vous pouvez nous écrire à contact@monprijet.com</p>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Accueil"
+};
+</script>
+
+<style scoped>
+.page {
+  padding: 20px;
+}
+</style>
